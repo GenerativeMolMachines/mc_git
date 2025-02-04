bc1db7192f9ec3e53fe12bdb37f55d408b23c23f
djzhendogg <djzhendogg.is.busy2@gmail.com>
2025-02-04 20:34:13 +0300

    m1k1k

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ def blosum62_encode(sequence):
     encoded_vector = []
     for i in range(len(sequence) - 1):
         pair = (sequence[i], sequence[i+1])
-        if pair in blosum62:
+        kmers = [sequence[i:i+k] for i in range(len(sequence) - k + 1)]
+
+            kmer_to_index = {kmer: idx for idx, kmer in enumerate([''.join(p) for p in product(amino_acids, repeat=k)])}
+                encoded = [kmer_to_index[kmer] for kmer in kmers]
+                    return encoded
             encoded_vector.append(blosum62[pair])
         elif (pair[1], pair[0]) in blosum62:
             encoded_vector.append(blosum62[(pair[1], pair[0])])
@@ -33,8 +37,13 @@ def process_dataset(df, encoding_func, encoding_name, pad_value):
     encoded_vector = []
 
     for dataset in benchmark_list:
-          dataset['length'] = dataset['seq'].apply(len)
-            length = dataset['length'].max()
-              print(length)
+        for i in range(len(sequence) - 1):
+                    pair = (sequence[i], sequence[i+1])
+                            if pair in blosum62:
+                                            encoded_vector.append(blosum62[pair])
+                                                    elif (pair[1], pair[0]) in blosum62:
+                                                                    encoded_vector.append(blosum62[(pair[1], pair[0])])
+                                                                            else:
+                                                                                            encoded_vector.append(0)print(length)
 
     return result_df
