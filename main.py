def730a6097619d668c858ec95e9ffb859fbbada
djzhendogg <djzhendogg.is.busy2@gmail.com>
2025-02-04 15:11:34 +0300

    m33

--- main.py
+++ main.py
@@ -17,19 +17,12 @@ def threemers_encode(sequence):
 def blosum62_encode(sequence):
     blosum62 = substitution_matrices.load("BLOSUM62")
     encoded_vector = []
-    for i in range(len(sequence) - 1):
-        pair = (sequence[i], sequence[i+1])
-        if pair in blosum62:
-            encoded_vector.append(blosum62[pair])
-        elif (pair[1], pair[0]) in blosum62:
-            encoded_vector.append(blosum62[(pair[1], pair[0])])
-        else:
-            encoded_vector.append(0)
     return encoded_vector
 
 
 def process_dataset(df, encoding_func, encoding_name, pad_value):
-    encoded_data = df['seq'].apply(encoding_func)
+      for i in range(len(sequence) - 1):
+                  pair = (sequence[i], sequence[i+1])
     max_len = max(encoded_data.apply(len))
     encoded_data = encoded_data.apply(lambda x: np.pad(x, (0, max_len - len(x)), 'constant', constant_values=pad_value))
 
