154007b71e72f6c9902d1ddaba55ad838ee5b387
Deimoz <davir222012@gmail.com>
2025-02-04 20:33:07 +0300

    w3.3

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def one_hot_encode(sequence):
-    encoder = OneHotEncoder(categories=[list(amino_acids)], dtype=int, sparse_output=False)
-    sequence_array = np.array(list(sequence)).reshape(-1, 1)
-    encoded = encoder.fit_transform(sequence_array).flatten()
+    blosum62 = substitution_matrices.load("BLOSUM62")
+    encoded_vector = []
+
+    for dataset in benchmark_list:
+        dataset['length'] = dataset['seq'].apply(len)
+        length = dataset['length'].max()
+        print(length)
     return encoded
 
 
@@ -14,27 +18,11 @@ def threemers_encode(sequence):
     return encoded
 
 
-def blosum62_encode(sequence):
-    blosum62 = substitution_matrices.load("BLOSUM62")
-    encoded_vector = []
-    for i in range(len(sequence) - 1):
-        pair = (sequence[i], sequence[i+1])
-        if pair in blosum62:
-            encoded_vector.append(blosum62[pair])
-        elif (pair[1], pair[0]) in blosum62:
-            encoded_vector.append(blosum62[(pair[1], pair[0])])
-        else:
-            encoded_vector.append(0)
-    return encoded_vector
-
-
 def process_dataset(df, encoding_func, encoding_name, pad_value):
-    blosum62 = substitution_matrices.load("BLOSUM62")
-    encoded_vector = []
+    k = 3
+    kmers = [sequence[i:i + k] for i in range(len(sequence) - k + 1)]
 
-    for dataset in benchmark_list:
-          dataset['length'] = dataset['seq'].apply(len)
-            length = dataset['length'].max()
-              print(length)
+    kmer_to_index = {kmer: idx for idx, kmer in enumerate([''.join(p) for p in product(amino_acids, repeat=k)])}
+    encoded = [kmer_to_index[kmer] for kmer in kmers]
 
     return result_df
